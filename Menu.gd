9f1c47468ccb596f3cd525a40fca6de3a2df170f
malle222 <122549298+malle222@users.noreply.github.com>
2024-05-24 15:53:56 +0200

    import_menu

---
+++ Menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_spielen_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_optionen_pressed():
+	get_tree().change_scene_to_file("res://options_menu.tscn")
+
+
+func _on_schließen_pressed():
+	get_tree().quit()
