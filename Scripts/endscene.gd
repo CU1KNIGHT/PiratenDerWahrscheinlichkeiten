314bfa98022ed1641d130c1fb093bf04fb472e24
mahmoud <mah2009moud@hotmail.com>
2024-06-29 02:00:34 +0200

    #14 fix button functions of end Scene

--- Scripts/endscene.gd
+++ Scripts/endscene.gd
@@ -15,19 +15,18 @@ func _process(delta):
 
 func _on_button_1_button_down():
 	get_tree().change_scene_to_file("res://Scenes/basic_islands_overview.tscn")
-	pass # Replace with function body.
+	
 
 
 func _on_button_2_button_down():
 	get_tree().change_scene_to_file("res://Scenes/ice_islands_overview.tscn")
-	pass # Replace with function body.
-
+	
 
 func _on_button_3_button_down():
 	get_tree().change_scene_to_file("res://Scenes/lava_islands_overview.tscn")
-	pass # Replace with function body.
+	
 
 
 func _on_button_4_button_down():
 	get_tree().quit()
-	pass # Replace with function body.
+	
