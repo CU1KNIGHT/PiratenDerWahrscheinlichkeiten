066bd886245df254a97255e60f323f610b714cc8
dappout <119581024+dappout@users.noreply.github.com>
2024-07-06 16:08:49 +0200

    Added pause button to Lava Islands scene

--- Scripts/lava_islands_overview.gd
+++ Scripts/lava_islands_overview.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+@onready var pause_menu = $PauseMenu
+var paused = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.global_current_overview_scene_path=Global.LAVA_OVERVIEW_SCENE_PATH
@@ -15,3 +16,14 @@ func _ready():
 
 func _on_nach_eis_insel_pressed():
 	get_tree().change_scene_to_file("res://scenes/ice_islands_overview.tscn")
+
+
+func _on_pause_pressed():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		
+	paused = !paused  
